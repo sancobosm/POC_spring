a1bc789da9b65bce9b4fe00e0f048ffdafef099d
Santiago Cobos <sancobosm@gmail.com>
2023-10-23 12:55:51 +0200

    First commit

---
+++ src/main/java/cobos/santiago/POC_SpringBatch/PocSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package cobos.santiago.POC_SpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cobos/santiago/POC_SpringBatch/PocSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package cobos.santiago.POC_SpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
